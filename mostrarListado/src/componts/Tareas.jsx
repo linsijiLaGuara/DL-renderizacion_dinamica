7c35fed482d7f327e815ae4cea215779f271a0ba
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-01-23 10:52:59 -0300

    pasar lista a mayusculas

--- mostrarListado/src/componts/Tareas.jsx
+++ mostrarListado/src/componts/Tareas.jsx
@@ -4,6 +4,8 @@
 const Tareas = () => {
     const listaTarea = ["Tarea 1", "Tarea 2", "Tarea 3"]
 
+    const lista = listaTarea.map(tarea => tarea.toLocaleUpperCase());
+    console.log(lista)
     return <ul>
         {listaTarea.map(tarea => <li key={tarea}>{tarea}</li>)}
     </ul>
